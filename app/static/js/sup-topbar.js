6679b58b695684e8aff4fa69d1a09bccea9c3cac
Kennya-42 <ksa6262@rit.edu>
2018-05-13 22:02:31 -0400

    top menu proto

---
+++ app/static/js/sup-topbar.js
@@ -0,0 +1,42 @@
+// ---------------------------------------------------------------
+// SlideUpTopBar for Foundation top-bar
+// ---------------------------------------------------------------
+
+var $window = $(window);
+var didScroll;
+var lastScrollTop = 0;
+var scrollAmount = 10;          // Value of scroll amount
+var navbarHeight = $('.slideUp').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    
+    "use strict";
+    
+    var sup = $(window).scrollTop();
+
+    if(Math.abs(lastScrollTop - sup) <= scrollAmount) return;
+
+    if (sup > lastScrollTop && sup > navbarHeight){
+        // On Scroll Down
+        $('.slideUp').css({top: -$(window).outerHeight()});
+    } else {
+        // On Scroll Up
+        if(sup + $(window).height() < $(document).height()) {
+            $('.slideUp').css({top: 0});
+        }
+    }
+
+    lastScrollTop = sup;
+
+}
